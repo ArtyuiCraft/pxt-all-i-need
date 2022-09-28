9cc1fd1301915a6bf7b902deb25b29b91b91712c
ArtyuiCraft <87765473+ArtyuiCraft@users.noreply.github.com>
2022-09-28 16:37:02 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,16 @@
+function x_y_handler (name_sprite: string, x: number, y: number) {
+    if (true) {
+    	
+    } else {
+    	
+    }
+}
+function create_sprite (name: string, x: number, y: number) {
+    spritelist.push(name)
+    x_y_handler(name, x, y)
+}
+let spritelist: string[] = []
+spritelist = []
+basic.forever(function () {
+	
+})
