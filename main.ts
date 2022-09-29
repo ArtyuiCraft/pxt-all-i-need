37a99188d647dff80485372ee343e00da7cc0a8b
ArtyuiCraft <87765473+ArtyuiCraft@users.noreply.github.com>
2022-09-29 15:45:59 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,37 @@
-function x_y_handler (name_sprite: string, x: number, y: number) {
-    if (true) {
-    	
+function create_sprite (name: string, x: number, y: number, background: boolean) {
+    spritelist.push(name)
+    x_y_handler(name, x, y, background)
+}
+function x_y_handler (name_sprite: string, x: number, y: number, background: boolean) {
+    register_A = spritelist.indexOf(name_sprite)
+    register_B = spritelist[register_A]
+    if (register_B == name_sprite) {
+        if (background == true) {
+            register_A = last_x.indexOf(name_sprite) + 1
+        } else {
+            led.plotBrightness(x, y, 255)
+            last_x.push("" + name_sprite + x)
+            last_y.push("" + name_sprite + y)
+        }
     } else {
-    	
+        basic.showLeds(`
+            . . . . .
+            # # # . #
+            # . # . #
+            # # # . #
+            . . . . .
+            `)
     }
 }
-function create_sprite (name: string, x: number, y: number) {
-    spritelist.push(name)
-    x_y_handler(name, x, y)
-}
+let register_B = ""
+let register_A = 0
+let last_x: string[] = []
+let last_y: string[] = []
 let spritelist: string[] = []
 spritelist = []
+create_sprite("test", 5, 1, true)
+last_y = []
+last_x = []
 basic.forever(function () {
 	
 })
